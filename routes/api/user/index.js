a626dc36c719d56a01273a40f9d06a2b19df4e5d
tandat198 <tandat198@gmail.com>
2020-07-08 10:44:19 +0700

    update

--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("./controller");
 const { authenticate } = require("../../../middlewares/auth");
 
-router.get("/get-saved-tutorials", authenticate, userController.getSavedTutorials);
+router.get("/saved-tutorials", authenticate, userController.getSavedTutorials);
 router.post("/sign-in", userController.signIn);
 router.post("/sign-up", userController.signUp);
 router.post("/save-tutorial", authenticate, userController.addTutorial);
