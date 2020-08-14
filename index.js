10f14e31335f9a5e23a1e7da8b669dba58554966
tandat198 <tandat198@gmail.com>
2020-08-14 18:46:55 +0700

    update

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const port = process.env.PORT || 5000;
 const app = express();
 const cors = require("cors");
 const { mongoURI } = require("./config");
+const whitelist = ["https://codeclass.vercel.app", "https://codeclassadmin.vercel.app"];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+};
 
 app.use(express.json({ extended: true, limit: "50mb" }));
-app.use("/api", cors(), require("./routes/api"));
+app.use("/api", cors(corsOptions), require("./routes/api"));
 mongoose.connect(
     mongoURI,
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false },
