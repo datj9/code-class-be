018d77304056dd660e04ac746dbae7d465ebea7b
Dat Nguyen <51848355+tandat198@users.noreply.github.com>
2020-08-17 06:50:59 +0700

    Update index.js

--- routes/api/mentor/index.js
+++ routes/api/mentor/index.js
@@ -6,9 +6,9 @@ const { authenticate, authorize } = require("../../../middlewares/auth");
 router.get("/", authenticate, authorize(["admin"]), mentorController.getMentors);
 router.get("/:mentorId", authenticate, authorize(["admin"]), mentorController.getOneMentor);
 router.get("/active-mentors", mentorController.getActiveMentors);
-router.get("active-mentors/:mentorId", mentorController.getOneActiveMentor);
+router.get("/active-mentors/:mentorId", mentorController.getOneActiveMentor);
 router.post("/", authenticate, authorize(["admin"]), mentorController.createMentor);
 router.put("/:mentorId", authenticate, authorize(["admin"]), mentorController.updateMentor);
-router.delete(":mentorId", authenticate, authorize(["admin"]), mentorController.deleteMentor);
+router.delete("/:mentorId", authenticate, authorize(["admin"]), mentorController.deleteMentor);
 
 module.exports = router;
