94f39fcb6c3a77d5d5ab32fed235c1d71421f88a
Dat Nguyen <51848355+tandat198@users.noreply.github.com>
2020-08-17 08:46:34 +0700

    Update index.js

--- routes/api/mentor/index.js
+++ routes/api/mentor/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const mentorController = require("./controller");
 const { authenticate, authorize } = require("../../../middlewares/auth");
 
+router.get("/active-mentors", mentorController.getActiveMentors);
 router.get("/", authenticate, authorize(["admin"]), mentorController.getMentors);
 router.get("/:mentorId", authenticate, authorize(["admin"]), mentorController.getOneMentor);
-router.get("/active-mentors", mentorController.getActiveMentors);
 router.get("/active-mentors/:mentorId", mentorController.getOneActiveMentor);
 router.post("/", authenticate, authorize(["admin"]), mentorController.createMentor);
 router.put("/:mentorId", authenticate, authorize(["admin", "mentor"]), mentorController.updateMentor);
